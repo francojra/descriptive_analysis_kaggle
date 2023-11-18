9dbc13592322d31fd5e14cca43c7ed5c9fd608ad
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-11-17 23:52:24 -0300

    Add files via upload

--- descriptive_analysis_kaggle.R
+++ descriptive_analysis_kaggle.R
@@ -5,9 +5,11 @@
 
 # Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
 
-library(dplyr)
+library(tidyverse)
 library(psych)
 library(stringi)
+library(hrbrthemes)
+library(viridis)
 
 # Carregar dados ---------------------------------------------------------------------------------------------------------------------------
 
@@ -51,3 +53,24 @@ describe(dados$teacher_student_relationship)
 describe(dados$social_support)
 describe(dados$bullying)
 describe(dados$stress_level)
+
+# Gráficos ---------------------------------------------------------------------------------------------------------------------------------
+
+data <- dados %>%
+  gather(key = "text", value = "value") %>%
+  mutate(text = gsub("\\.", " ",text)) %>%
+  mutate(text = fct_reorder(text, value)) %>%
+
+
+ggplot(aes(x = value, color = text, fill = text)) +
+  geom_histogram(alpha = 0.6, binwidth = 5) +
+  scale_fill_viridis(discrete = TRUE) +
+  scale_color_viridis(discrete = TRUE) +
+  theme_ipsum() +
+  theme(
+    legend.position = "none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)) +
+  xlab("") +
+  ylab("Assigned Probability (%)") +
+  facet_wrap(~text)
